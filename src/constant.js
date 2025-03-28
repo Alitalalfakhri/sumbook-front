138fc627e1f380ae7db6895bfa1b865946156cc4
Alitalalfakhri <ali.talalfakhri09@gmail.com>
2025-03-28 16:56:41 +0300

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,1 @@
-export const backendUrl = "https://d3d84c17-d5e4-45a5-8ef1-4baf64db86fd-00-24dxun34z9o1a.sisko.replit.dev"
+export const backendUrl = "https://sumbook-back-production-c036.up.railway.app"
