24fa48c4b5e8e8a88255ee4cfee818c936b17e13
Alitalalfakhri <ali.talalfakhri09@gmail.com>
2025-03-28 16:57:08 +0300

    Update urlSlice.js

--- src/features/generals/urlSlice.js
+++ src/features/generals/urlSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const urlSlice = createSlice({
   name:"serverUrl",
   initialState:{
-    url:"https://d3d84c17-d5e4-45a5-8ef1-4baf64db86fd-00-24dxun34z9o1a.sisko.replit.dev",
+    url:"https://sumbook-back-production-c036.up.railway.app",
   },
 
 })
